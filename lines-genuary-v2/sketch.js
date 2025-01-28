aa48c35d0b14b59b455ce54f75cc07ae55a83d36
sorensorel <taha.skiredj@gmail.com>
2025-01-28 14:47:29 -0500

    Genuary-line new version

---
+++ lines-genuary-v2/sketch.js
@@ -0,0 +1,35 @@
+//Mix between Jan 1 (Vertical lines only), JAN 12 (subdivision) and JAN 14 (Pure black and white)
+// Inspiré de l'esthétique de Ryoji Ikeda
+
+function setup() {
+  var width = 1000;
+  var height = 1000;
+  createCanvas(width, height);
+}
+
+function draw() {
+  noLoop();
+  background("black");
+  var subdivision;
+  subdivision = random(1, 10);
+  subdivision = round(subdivision);
+  var incrementX = width / (10 * subdivision);
+  var incrementY = height / (20 * subdivision);
+  var positionX;
+  var positionY;
+  var linesWidth;
+  var lentgh = incrementY;
+
+  for (let x = 0; x <= width; x += incrementX) {
+    for (let y = 0; y <= height; y += incrementY) {
+      var linesWidth;
+      linesWidth = random([1, 2, 3]);
+      positionX = random(x, x + incrementX);
+      positionY = y;
+      stroke("white");
+      strokeWeight(linesWidth);
+
+      line(positionX, positionY, positionX, positionY + lentgh);
+    }
+  }
+}
